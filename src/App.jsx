028f75c24b0edc849276a6abcd0806db5a829eea
Gamze <gencoglugamze@gmail.com>
2025-07-30 13:45:35 +0200

    gruppen komponent und änderungen

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,25 @@ import About from "./components/About.jsx";
 import Wanderer from "./components/Wanderer.jsx";
 import Touren from "./components/Touren.jsx";
 import Gruppen from "./components/Gruppen.jsx";
+import {useState} from "react";
 
 function App() {
+    const [wandererList, setWandererList] = useState([
+        {
+            id: 1,
+            name: 'Gandalf',
+            surname: 'Grauer',
+            phone: '',
+            address: ''
+        },
+        {
+            id: 2,
+            name: 'Loki',
+            surname: '',
+            phone: '123456789',
+            address: 'Asgard'
+        }
+    ]);
 
   return (
     <>
@@ -16,9 +33,11 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About/>} />
-            <Route path="/wanderer" element={<Wanderer />} />
+            <Route path="/wanderer" element={<Wanderer
+            wandererList={wandererList} setWandererList={setWandererList} />} />
             <Route path="/touren" element={<Touren />} />
-            <Route path="/gruppen" element={<Gruppen />} />
+            <Route path="/gruppen" element={<Gruppen
+            wandererList={wandererList}/>} />
 
 
         </Routes>
